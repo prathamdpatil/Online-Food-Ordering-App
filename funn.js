3b367d45338ec97eb078c95a3412305621e4b35f
prathamesh <prathmeshsonwane1309@gmail.com>
2024-08-01 14:36:30 +0530

    third

--- funn.js
+++ funn.js
@@ -1,4 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const head = React.createElement("h1",{},"hii pratham");
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(head);
-console.log(headd);
+console.log(head);
